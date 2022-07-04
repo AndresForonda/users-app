2e2b253260c52c24c907e5eb3f141aecc790614e
Andres Foronda <andresforonda.af@gmail.com>
2022-07-04 15:41:41 -0500

    Add TableFlex and TableHeader components

---
+++ src/components/table/TableFlex.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { GenericObject } from '@/models/generics'
+import { PropType } from 'vue'
+import TableHeader from './TableHeader.vue'
+defineProps({
+  tableHeaders: {
+    type: Object as PropType<GenericObject>,
+    required: true,
+  },
+})
+</script>
+<template>
+  <div class="flex-table" data-testid="flex-table" id="flex-table">
+    <table-header :table-headers="tableHeaders" />
+    <slot name="table-body" />
+  </div>
+</template>
---
+++ src/components/table/TableHeader.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { GenericObject } from '@/models/generics'
+import { PropType } from 'vue'
+
+defineProps({
+  tableHeaders: {
+    type: Object as PropType<GenericObject>,
+    required: true,
+  },
+})
+</script>
+
+<style></style>
+
+<template>
+  <div class="row header" data-testid="users-table-header">
+    <div class="cell" v-for="header in tableHeaders">{{ header }}</div>
+  </div>
+</template>

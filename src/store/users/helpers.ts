2bf64ef2f265eea0c147695da2e9a5d174ef687b
Andres Foronda <andresforonda.af@gmail.com>
2022-07-03 20:15:19 -0500

    Add getPersistentData helper to users helpers

--- src/store/users/helpers.ts
+++ src/store/users/helpers.ts
@@ -1,3 +1,9 @@
 export const persistData = (key: string, value: string) => {
   localStorage.setItem(key, value)
 }
+
+export const getPersistentData = (key: string) => {
+  const item = localStorage.getItem(key)
+  if (item) return JSON.parse(item)
+  return null
+}

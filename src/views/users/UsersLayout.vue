d4fe51273cbfd9ff2045a9159b2e25c091634996
Andres Foronda <andresforonda.af@gmail.com>
2022-07-04 15:45:50 -0500

    Add UsersLayout and use it in App.vue

---
+++ src/views/users/UsersLayout.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import FlexTable from '@/components/table/FlexTable.vue'
+import { useUsers } from './useUsers'
+import UserTableRow from './UsersTableRow.vue'
+const { filterWord, users } = useUsers()
+const baseTableHeaders = {
+  name: 'Name',
+  username: 'Username',
+  email: 'Email',
+  actions: 'Actions',
+}
+</script>
+<template>
+  Filter
+  <input v-model="filterWord" />
+  <flex-table :table-headers="baseTableHeaders">
+    <template v-slot:table-body>
+      <UserTableRow v-for="user in users" :key="user.id" :userInfo="user" />
+    </template>
+  </flex-table>
+  <!--  -->
+</template>

56260564a64cc5b19871cd1b634e5d28c26dec54
Andres Foronda <andresforonda.af@gmail.com>
2022-07-03 20:19:00 -0500

    Register actions and mutations for users module

--- src/store/users/index.ts
+++ src/store/users/index.ts
@@ -1,6 +1,10 @@
+import actions from './usersActions'
+import mutations from './usersMutations'
 import state from './usersState'
 
 export default {
-  state,
+  actions,
+  mutations,
   namespaced: true,
+  state,
 }

0f5ca091bf6c5e78c8c39b7e8805b512b8ed7590
Andres Foronda <andresforonda.af@gmail.com>
2022-07-04 15:45:04 -0500

    Add comments to useUsers composable

--- src/views/users/useUsers.ts
+++ src/views/users/useUsers.ts
@@ -2,21 +2,23 @@ import { UserInterface } from '@/models/users'
 import { useStore } from '@/store'
 import { computed, ref } from 'vue'
 
-const filterWord = ref('')
-const showEditModal = ref(false)
-const userIdToEdit = ref<number>()
+const filterWord = ref('') // used to filter the table registers
+const showEditModal = ref(false) // used to hide/show edit modal
+const userIdToEdit = ref<number>() // user id for the user to be edited
 
 export const useUsers = () => {
   const store = useStore()
-  store.dispatch('users/getUsers')
+  store.dispatch('users/getUsers') // request users from store action
   const users = computed<UserInterface[]>(() => {
     if (filterWord.value)
+      // filter the users array using filter word, filter is applied to name, username and email
       return store.state.users.users.filter(
         (user) =>
           user.name.includes(filterWord.value) ||
           user.username.includes(filterWord.value) ||
           user.email.includes(filterWord.value),
       )
+    // return users state if there is not any word fo filter
     return store.state.users.users
   })
   return {

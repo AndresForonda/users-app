cfc56e58840d4757a7d699857dcd3ea4679830be
Andres Foronda <andresforonda.af@gmail.com>
2022-07-04 14:48:11 -0500

    Add basic css theme

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { store, key } from '@/store'
 import App from './App.vue'
+import '@/assets/css/theme.css'
 
 const app = createApp(App)
 app.use(store, key)

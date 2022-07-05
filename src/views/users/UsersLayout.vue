4a612a3d3c2db0315cf878f57e43dddb6451414a
Andres Foronda <andresforonda.af@gmail.com>
2022-07-05 01:10:23 -0500

    Add UsersEditModal component to UsersLayout

--- src/views/users/UsersLayout.vue
+++ src/views/users/UsersLayout.vue
@@ -2,6 +2,7 @@
 import TableFlex from '@/components/table/TableFlex.vue'
 import { useUsers } from './useUsers'
 import UserTableRow from './UsersTableRow.vue'
+import UsersEditModal from './UsersEditModal.vue'
 const { filterWord, users } = useUsers()
 const baseTableHeaders = {
   name: 'Name',
@@ -15,20 +16,26 @@ const baseTableHeaders = {
 .table-body-wrapper {
   border: 1px solid var(--color-gray);
 }
+.users-layout-wrapper {
+  margin-top: 60px;
+}
 </style>
 <template>
-  Filter
-  <input v-model="filterWord" />
-  <table-flex :table-headers="baseTableHeaders">
-    <template v-slot:table-body>
-      <div class="table-body-wrapper mx-10">
-        <user-table-row
-          v-for="user in users"
-          :key="user.id"
-          :userInfo="user"
-          :table-headers="baseTableHeaders"
-        />
-      </div>
-    </template>
-  </table-flex>
+  <div class="users-layout-wrapper">
+    Filter
+    <input v-model="filterWord" />
+    <users-edit-modal />
+    <table-flex :table-headers="baseTableHeaders">
+      <template v-slot:table-body>
+        <div class="table-body-wrapper mx-10">
+          <user-table-row
+            v-for="user in users"
+            :key="user.id"
+            :userInfo="user"
+            :table-headers="baseTableHeaders"
+          />
+        </div>
+      </template>
+    </table-flex>
+  </div>
 </template>

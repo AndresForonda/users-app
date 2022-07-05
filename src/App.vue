cf2562d329e0a0df83c513d7c9388bc70f7ca419
Andres Foronda <andresforonda.af@gmail.com>
2022-07-05 01:10:41 -0500

    Remove margin from root App component

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,6 @@ import UsersLayout from './views/users/UsersLayout.vue'
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
   overflow-y: hidden;
 }
 </style>
